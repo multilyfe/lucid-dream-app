6a0fd8fb4e7adcb0b7366b84c78eb798689857f2
Multilyfe <ldlfaiow@gmail.com>
2025-09-18 06:13:12 -0400

    🛡️ Disable ESLint in production for Vercel build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
-export default nextConfig;
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
+
+module.exports = nextConfig
